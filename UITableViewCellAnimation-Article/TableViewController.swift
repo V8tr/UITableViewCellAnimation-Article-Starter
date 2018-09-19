0fcf76b0ea8f2a69eed6b37e60611aef3d2fef8a
V8tr <ne0_n@mail.ru>
2018-09-19 16:17:18 +0300

    Initial commit

---
+++ UITableViewCellAnimation-Article/TableViewController.swift
@@ -0,0 +1,23 @@
+//
+//  TableViewController.swift
+//  UITableViewCellAnimation-Article
+//
+//  Created by Vadym Bulavin on 9/4/18.
+//  Copyright © 2018 Vadim Bulavin. All rights reserved.
+//
+
+import UIKit
+
+class TableViewController: UITableViewController {
+
+	@IBAction func onRefresh(_ sender: UIBarButtonItem) {
+		// Refresh table view here
+	}
+
+	// MARK: - Table view data source and delegate methods
+
+	override func tableView(_ tableView: UITableView, willDisplay cell: UITableViewCell, forRowAt indexPath: IndexPath) {
+		// Add animations here
+	}
+}
+
